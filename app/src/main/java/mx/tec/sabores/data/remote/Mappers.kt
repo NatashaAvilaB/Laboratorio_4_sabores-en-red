4d353b9b245f0864250bab6f2571a7517f483da0
NatashaAvilaB <natashaavbasti@gmail.com>
2026-09-09 11:07:38 -0600

    checkpoint a4: lista desde el servidor

--- app/src/main/java/mx/tec/sabores/data/remote/Mappers.kt
+++ app/src/main/java/mx/tec/sabores/data/remote/Mappers.kt
@@ -2,7 +2,10 @@ package mx.tec.sabores.data.remote
 
 import mx.tec.sabores.domain.Restaurant
 import mx.tec.sabores.domain.Review
+import mx.tec.sabores.domain.RatingSummary
 
+/** El promedio que el servidor ya calculó, en la forma que entiende el dominio. */
+fun RestaurantDto.toSummary() = RatingSummary(average = ratingAverage, count = ratingCount)
 fun RestaurantDto.toDomain() = Restaurant(
     id = id,
     name = name,
